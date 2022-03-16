c3cefa4b0b00c63bbda677bd6bf08328df2c9bc5
Ifycode <marychord@gmail.com>
2022-03-16 02:27:35 +0100

    Initial code with App class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      App works!
-    </div>
-  );
+class App extends Component {
+  state = {
+    user: {
+      username: 'jioke',
+      firstName: 'Kingsley',
+      lastName: 'Silas'
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <User user={this.state.user} />
+      </div>
+    )
+  }
 }
+
+const User = (props) => (
+  <div>
+    <UserProfile {...props.user} />
+  </div>
+)
+
+const UserProfile = (props) => (
+  <div>
+    <h2>Profile Page of {props.username}</h2>
+    <UserDetails {...props} />
+  </div>
+)
+
+const UserDetails = (props) => (
+  <div>
+    <p>Username: {props.username}</p>
+    <p>First Name: {props.firstName}</p>
+    <p>Last Name: {props.lastName}</p>
+  </div>
+)
 
 export default App;
