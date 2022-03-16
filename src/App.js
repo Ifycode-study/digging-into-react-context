1c578f4125219154d97756093d1cc83325256941
Ifycode <marychord@gmail.com>
2022-03-16 03:41:48 +0100

    context API to the rescue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import { useState } from 'react';
+import { createContext, useState } from 'react';
+
+const UserContext = createContext({});
+const UserProvider = UserContext.Provider;
+const UserConsumer = UserContext.Consumer;
 
 function App() {
   const [user, setUser] = useState({
@@ -8,30 +12,44 @@ function App() {
   });
   return (
     <div>
-    <User user={user} />
+      <UserProvider value={user}>
+        <User />
+      </UserProvider>
   </div>
   );
 }
 
-const User = ({ user }) => (
+const User = () => (
   <div>
-    <UserProfile {...user} />
+    <UserProfile />
   </div>
 )
 
-const UserProfile = ({ ...user }) => (
-  <div>
-    <h2>Profile Page of {user.username}</h2>
-    <UserDetails {...user} />
-  </div>
+const UserProfile = () => (
+  <UserConsumer>
+    {context => {
+      return (
+        <div>
+          <h2>Profile Page of {context.username}</h2>
+          <UserDetails />
+        </div>
+      );
+    }}
+  </UserConsumer>
 )
 
-const UserDetails = ({ ...user }) => (
-  <div>
-    <p>Username: {user.username}</p>
-    <p>First Name: {user.firstName}</p>
-    <p>Last Name: {user.lastName}</p>
-  </div>
+const UserDetails = () => (
+  <UserConsumer>
+  {context => {
+    return (
+      <div>
+        <p>Username: {context.username}</p>
+        <p>First Name: {context.firstName}</p>
+        <p>Last Name: {context.lastName}</p>
+    </div>
+    );
+  }}
+</UserConsumer>
 )
 
 export default App;
